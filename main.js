d14035af63916c1b744de13d8ef4879e35c488be
Mario Teves <92680256+martevesjr@users.noreply.github.com>
2023-09-16 19:10:30 +0700

    edited main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+<script>
+  function myName()
+  {
+    <button
+      type="button"
+      onclick="document.getElementById('demo').innerHTML = Date()"
+    >
+      Click me to display Date and Time.
+    </button>
+  }
+</script>;
